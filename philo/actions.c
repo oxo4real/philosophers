84fb028cf1602fc064a1401e19884effc7c0670d
oxo4real <abduredoxo@hotmail.com>
2025-01-30 18:15:14 +0100

    update

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 19:53:06 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/01/28 20:24:07 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/01/29 15:11:48 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ void	rest(t_philo *philo)
 	unsigned long long	curr_time;
 	unsigned long long	elapsed_time;
 
+	pthread_mutex_lock(&philo->data->print);
 	gettimeofday(&time, NULL);
 	curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
 	elapsed_time = curr_time - philo->data->start_time;
 	print_action(elapsed_time, philo, "is sleeping\n");
+	pthread_mutex_unlock(&philo->data->print);
 	usleep(philo->data->time_to_sleep * 1000);
 }
 
@@ -31,10 +33,12 @@ void	think(t_philo *philo)
 	unsigned long long	curr_time;
 	unsigned long long	elapsed_time;
 
+	pthread_mutex_lock(&philo->data->print);
 	gettimeofday(&time, NULL);
 	curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
 	elapsed_time = curr_time - philo->data->start_time;
 	print_action(elapsed_time, philo, "is thinking\n");
+	pthread_mutex_unlock(&philo->data->print);
 }
 
 void	take_a_fork(t_philo *philo)
@@ -43,10 +47,12 @@ void	take_a_fork(t_philo *philo)
 	unsigned long long	curr_time;
 	unsigned long long	elapsed_time;
 
+	pthread_mutex_lock(&philo->data->print);
 	gettimeofday(&time, NULL);
 	curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
 	elapsed_time = curr_time - philo->data->start_time;
 	print_action(elapsed_time, philo, "has taken a fork\n");
+	pthread_mutex_unlock(&philo->data->print);
 }
 
 void	eat(t_philo *p)
@@ -60,9 +66,10 @@ void	eat(t_philo *p)
 	take_a_fork(p);
 	pthread_mutex_lock(&p->data->forks[max(p->id % n, (p->id + 1) % n)]);
 	take_a_fork(p);
-	pthread_mutex_lock(&p->access);
+	pthread_mutex_lock(&p->data->print);
+	// pthread_mutex_lock(&p->access);
 	p->is_eating = true;
-	pthread_mutex_unlock(&p->access);
+	// pthread_mutex_unlock(&p->access);
 	gettimeofday(&time, NULL);
 	elapsed = time.tv_sec * 1000 + time.tv_usec / 1000 - p->data->start_time;
 	print_action(elapsed, p, "is eating\n");
@@ -70,11 +77,12 @@ void	eat(t_philo *p)
 	pthread_mutex_unlock(&p->data->forks[max(p->id % n, (p->id + 1) % n)]);
 	pthread_mutex_unlock(&p->data->forks[min(p->id % n, (p->id + 1) % n)]);
 	gettimeofday(&time, NULL);
-	pthread_mutex_lock(&p->access);
+	// pthread_mutex_lock(&p->access);
 	p->last_eaten = time.tv_sec * 1000 + time.tv_usec / 1000;
 	p->is_eating = false;
 	p->meals_eaten++;
-	pthread_mutex_unlock(&p->access);
+	pthread_mutex_unlock(&p->data->print);
+	// pthread_mutex_unlock(&p->access);
 }
 
 void	die(t_philo *philo)
@@ -83,10 +91,10 @@ void	die(t_philo *philo)
 	unsigned long long	curr_time;
 	unsigned long long	elapsed_time;
 
+	pthread_mutex_lock(&philo->data->print);
 	gettimeofday(&time, NULL);
 	curr_time = time.tv_sec * 1000 + time.tv_usec / 1000;
 	elapsed_time = curr_time - philo->data->start_time;
-	pthread_mutex_lock(&philo->data->print);
 	ft_putnbr_fd(elapsed_time, 1);
 	ft_putchar_fd(' ', 1);
 	ft_putnbr_fd(philo->id, 1);
--- philo/azrael.c
+++ philo/azrael.c
@@ -6,14 +6,15 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 20:30:20 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/01/28 20:23:21 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/01/29 11:28:40 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <pthread.h>
 
 unsigned long long	get_tm(void);
-void				kill_philo(t_philo *p, t_data *d);
+void				kill_philo(t_philo *p, t_philo *d);
 void				set_data(t_data *d);
 
 void	azrael(t_philo *p, t_data *d)
@@ -33,7 +34,7 @@ void	azrael(t_philo *p, t_data *d)
 			if (get_tm() - p[i].last_eaten > d->time_to_die && !p[i].is_eating
 				&& !p[i].stop)
 			{
-				kill_philo(&p[i], d);
+				kill_philo(&p[i], p);
 				if (!d->meals)
 					return (pthread_mutex_unlock(&p[i].access),
 						(void)pthread_mutex_unlock(&p->death));
@@ -53,18 +54,23 @@ unsigned long long	get_tm(void)
 	return (t.tv_sec * 1000 + t.tv_usec / 1000);
 }
 
-void	kill_philo(t_philo *p, t_data *d)
+void	kill_philo(t_philo *p, t_philo *d)
 {
-	if (!d->meals)
-	{
-		pthread_mutex_lock(&d->death);
-		d->stop = true;
-		pthread_mutex_unlock(&d->death);
-	}
+	unsigned long long	i;
+
+	pthread_mutex_lock(&d->data->print);
 	die(p);
-	pthread_mutex_lock(&p->death);
 	p->stop = true;
-	pthread_mutex_unlock(&p->death);
+	if (!d->data->meals)
+	{
+		i = 0;
+		while (i < d->data->philo_num)
+		{
+			d[i].stop = true;
+			i++;
+		}
+	}
+	pthread_mutex_unlock(&d->data->print);
 }
 
 void	set_data(t_data *d)
